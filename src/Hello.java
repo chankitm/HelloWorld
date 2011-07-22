3cf51097a98c415c3ffea0e29f9d10cd33d76c11
Chan, Roy KM <Roy.KM.Chan@pccw.com>
2011-07-22 14:40:11 +0800

    add function: outdated()
    Signed-off-by: Chan, Roy KM <Roy.KM.Chan@pccw.com>

--- src/Hello.java
+++ src/Hello.java
@@ -9,6 +9,12 @@ public class Hello {
 		System.out.println("***HelloWorld!!!***");
 		System.out.println("***Version 2!***");
 		System.out.println("***Version 3!***");
+		
+		outdated();
 	}
 
+	
+	public static void outdated(){
+		
+	}
 }
